74c814b1db821a3c573e70cbe23d8e45b4b6e0cd
umarani-design <77783395+umarani-design@users.noreply.github.com>
2021-01-25 03:36:36 -0500

    Assignment 3 submitted

---
+++ Assignment3/question1.js
@@ -0,0 +1,26 @@
+var library = [
+    {
+    author: 'Bill Gates',
+    title: 'The Road Ahead',
+    readingStatus: true
+    },
+    {
+    author: 'Steve Jobs',
+    title: 'Walter Isaacson',
+    readingStatus: true
+    },
+    {
+    author: 'Suzanne Collins',
+    title: 'Mockingjay: The Final Book of The Hunger Games',
+    readingStatus: false
+    }];
+
+    for(var i=0; i<library.length; i++){
+        if(library[i].readingStatus===true)
+        {
+            console.log("Already read"+ "'"+library[i].author+"'" +"by the"+library[i].title+".");
+        }
+        else{
+        console.log("you still need to read"+"'"+library[i].title+"'"+"by the"+"'"+library[i].author+".");
+        }
+    }
